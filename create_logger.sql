e12508ae104a72470032b333e52f94f4cf90fb01
= <kdubartell@gmail.com>
2023-10-15 18:32:46 -0600

    created logger proc

--- create_logger.sql
+++ create_logger.sql
@@ -15,7 +15,7 @@ CREATE or replace function raw.logger(
 
     begin 
 
-    v_dyn_sql := 'insert into raw.log_fact values('''||procedure_name||''', '''||step_name||''' ,'||step_number||', '''||message||''', '||rows_affected||' ,'||duration_seconds||') on conflict do nothing';
+    v_dyn_sql := 'insert into raw.log_fact '''||procedure_name||''', '''||step_name||''' ,'||step_number||', '''||message||''', '||rows_affected||' ,'||duration_seconds||' on conflict do nothing';
     execute v_dyn_sql;
     return TRUE;
     end;
